ac79dda1e51cbc1247ff525a8ff3ec4560274c8b
gabhiinav <gabhiinav@gmail.com>
2023-08-10 20:15:50 +0530

    Initial Commit

---
+++ OGDex/OGDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  OGDexApp.swift
+//  OGDex
+//
+//  Created by Abhinav Gupta on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct OGDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
