bbd78f4cad0fd1cbf2bc5bdbe9f457c7207eb1ac
gabhiinav <gabhiinav@gmail.com>
2023-08-10 20:33:24 +0530

    Model

--- OGDex/Models/PokemonModel.swift
+++ OGDex/Models/PokemonModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct PokemonPage {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "mew", url: "https://pokeapi.co/api/v2/pokemon/151/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
