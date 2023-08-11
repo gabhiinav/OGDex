f27dcf2d92a0f0116a3df670b809947143d0175d
gabhiinav <gabhiinav@gmail.com>
2023-08-11 15:45:19 +0530

    Views

--- OGDex/Views/ContentView.swift
+++ OGDex/Views/ContentView.swift
@@ -7,17 +7,36 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    @StateObject var vm = ViewModel()
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)
+                        ) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeInOut(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("PokemonUI")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
         }
-        .padding()
+        .environmentObject(vm)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
